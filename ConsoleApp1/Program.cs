54d766f528f1b076a645ea26d9b96c073768c868
xusanov10 <ahmadjonxusanovv>
2025-04-10 16:35:08 +0500

    change2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -28,3 +28,31 @@ static void Main(string[] args)
         Console.WriteLine();
     }
 }
+
+static void Main1(string[] args)
+{
+    int[][] jaggedArray = new int[3][]
+    {
+               new int []  {1, 2, 3},
+               new int []  {4, 5, 6},
+               new int []  {7, 8, 9}
+    };
+
+    for (int i = 0; i < jaggedArray.Length; i++)
+    {
+        for (int j = 0; j < jaggedArray[i].Length; j++)
+        {
+            Console.Write(jaggedArray[i][j] + " ");
+        }
+        Console.WriteLine();
+    }
+    for (int i = 0; i < jaggedArray.Length; i++)
+    {
+        int counter = 0;
+        for (int j = 0; j < jaggedArray[i].Length; j++)
+        {
+            counter += jaggedArray[i][j];
+        }
+        Console.WriteLine($"{i + 1} qator: " + counter);
+    }
+}
