53d1dac805ca5e94c16ab46f6670a97f84be8806
xusanov10 <ahmadjonxusanovv>
2025-04-10 16:34:06 +0500

    gange1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+static void Main(string[] args)
+{
+    int[,,] ints = new int[2, 2, 2];
+    int value = 1;
+
+    for (int i = 0; i < ints.GetLength(0); i++)
+    {
+        for (int j = 0; j < ints.GetLength(1); j++)
+        {
+            for (int k = 0; k < ints.GetLength(2); k++)
+            {
+                ints[i, j, k] = value++;
+            }
+        }
+    }
+    for (int i = 0; i < ints.GetLength(0); i++)
+    {
+        for (int j = 0; j < ints.GetLength(1); j++)
+        {
+            for (int k = 0; k < ints.GetLength(2); k++)
+            {
+                Console.Write(ints[i, j, k] + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
