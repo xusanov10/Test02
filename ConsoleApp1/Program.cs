367599ede6cf17755b40a7f8e896cbb49ef009c7
xusanov10 <ahmadjonxusanovv>
2025-04-10 21:23:03 +0500

    change10

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -139,3 +139,4 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
